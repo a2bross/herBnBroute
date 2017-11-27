f2514b7d893fe9cb1e1073e2de1b4583c9fdaee8
Nicolas Lizop <nicolas.lizop@gmail.com>
2017-11-27 16:35:45 +0100

    added methods new and create to plots controller

--- app/controllers/plots_controller.rb
+++ app/controllers/plots_controller.rb
@@ -1,2 +1,19 @@
 class PlotsController < ApplicationController
+  def new
+    @plot = Plot.new
+  end
+
+  def create
+    @plot = Plot.new(plot_params)
+    @plot.save
+
+    redirect_to 'plots#show'
+  end
+
+  private
+
+  def plot_params
+    params.require(:plot).permit(:capacity, :name, :description, :location, :daily_price, :photo, :user_id)
+  end
+
 end
