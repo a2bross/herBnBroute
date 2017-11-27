bbac78c23460769ef161f72e947b1c00c722b38c
Nicolas Lizop <nicolas.lizop@gmail.com>
2017-11-27 17:55:11 +0100

    finished add a plot functionality (with views)

--- app/controllers/plots_controller.rb
+++ app/controllers/plots_controller.rb
@@ -1,19 +1,33 @@
 class PlotsController < ApplicationController
+
+  # index et show sont temporaires
+  def index
+    @plots = Plot.all
+  end
+  #
+  def show
+    @plot = Plot.find(params[:id])
+  end
+  # end temp
+
   def new
     @plot = Plot.new
   end
 
   def create
     @plot = Plot.new(plot_params)
-    @plot.save
-
-    redirect_to 'plots#show'
+    @plot.user = current_user
+    if @plot.save
+      redirect_to plot_path(@plot)
+    else
+      raise
+    end
   end
 
   private
 
   def plot_params
-    params.require(:plot).permit(:capacity, :name, :description, :location, :daily_price, :photo, :user)
+    params.require(:plot).permit(:capacity, :name, :description, :location, :daily_price, :photo, :user_id)
   end
 
 end
