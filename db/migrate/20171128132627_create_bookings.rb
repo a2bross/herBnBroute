faa805ce3256d9e5bc56b5d2b128f2e5a5a9ff97
Nicolas Lizop <nicolas.lizop@gmail.com>
2017-11-28 15:46:48 +0100

    added missing migration

--- db/migrate/20171128132627_create_bookings.rb
+++ db/migrate/20171128132627_create_bookings.rb
@@ -2,10 +2,10 @@ class CreateBookings < ActiveRecord::Migration[5.1]
   def change
     create_table :bookings do |t|
       t.integer :user_id
-      t.string :plot_id=integer
+      t.integer :plot_id
       t.date :start_date
       t.date :end_date
-      t.real :full_price
+      t.integer :full_price
       t.integer :review_id
       t.string :status
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171127142106) do
+ActiveRecord::Schema.define(version: 20171128132627) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "plot_id"
+    t.date "start_date"
+    t.date "end_date"
+    t.integer "full_price"
+    t.integer "review_id"
+    t.string "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "plots", force: :cascade do |t|
     t.string "name"
