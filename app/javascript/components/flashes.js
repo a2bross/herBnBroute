8bc84313bed85c8e09f13341a13db251bf91e04f
Zoe Cade <zoe.cade@gmail.com>
2017-11-28 18:28:21 +0100

    js fix

--- app/javascript/components/flashes.js
+++ app/javascript/components/flashes.js
@@ -1,6 +1,8 @@
 function hideFlashAfter5sec() {
   const alert = document.querySelector(".alert-dismissible");
-  setTimeout(() => {alert.style.display = "none"}, 3000)
+  if (alert) {
+    setTimeout(() => {alert.style.display = "none"}, 3000);
+  }
 };
 
 export { hideFlashAfter5sec };
