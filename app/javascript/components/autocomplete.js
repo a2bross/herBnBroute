9cbda4c7a6486bd068dc353c73abc6ce4cccaf1c
Agathe Loubert <agaloubert@hotmail.com>
2017-11-29 12:23:36 +0100

    fixed geocoding bug

--- app/javascript/components/autocomplete.js
+++ app/javascript/components/autocomplete.js
@@ -2,7 +2,7 @@ function autocomplete() {
   document.addEventListener("DOMContentLoaded", function() {
     var plotLocation = document.getElementById('plot_location');
 
-    if (plotlocation) {
+    if (plotLocation) {
       var autocomplete = new google.maps.places.Autocomplete(plotLocation, { types: [ 'geocode' ] });
       google.maps.event.addDomListener(plotLocation, 'keydown', function(e) {
         if (e.key === "Enter") {
