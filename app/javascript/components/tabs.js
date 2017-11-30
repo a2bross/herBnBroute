5fc9bbb9f15331ad878fdb8c396df7d8c1551083
Augustin de Brosses <augustin.debrosses@gmail.com>
2017-11-30 14:45:53 +0100

    added some JS in partial

--- app/javascript/components/tabs.js
+++ app/javascript/components/tabs.js
@@ -1,1 +1,26 @@
 
+// sélectionner les tabs
+const tabs = document.querySelectorAll(".tab");
+// boucler sur les tabs
+tabs.forEach((tab) => {
+  // poser un micro sur chaque tab (click)
+  tab.addEventListener("click", (event) => {
+    event.preventDefault();
+    // supprimer la classe active des autres tabs
+    tabs.forEach((item) => {
+      item.classList.remove("active");
+    });
+    // ajouter la classe active au currentTarget
+    event.currentTarget.classList.add("active");
+    // ajouter la classe hidden aux autres div.tabcontent
+    document.querySelectorAll('.tabcontent').forEach((div) => {
+      div.classList.add("hidden");
+    });
+    // récupérer le href du currentTarget
+    const id = event.currentTarget.href.split('#')[1];
+    // recupérer la div correspondante
+    const targetDiv = document.getElementById(id);
+    // lui enlever la classe hidden
+    targetDiv.classList.remove('hidden');
+  })
+});
