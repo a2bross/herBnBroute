2e4450f8be63184127bb5a4b9676ca51544c8762
Zoe Cade <zoe.cade@gmail.com>
2017-11-30 15:49:22 +0100

    added price to plot show

---
+++ app/javascript/components/total_price.js
@@ -0,0 +1,27 @@
+const showPrice = (duration) => {
+  const price = document.getElementById('total_price');
+  if (price) {
+    const daily_price = price.dataset.dailyprice;
+    price.innerText = `Total price: ${daily_price * (duration + 1)}€`;
+  };
+};
+
+const calculatePrice = (event) => {
+  const start_date = document.getElementById('booking_start_date');
+  const end_date = document.getElementById('booking_end_date');
+  if (start_date.value !== "" && end_date.value !== "") {
+    const duration = (Date.parse(end_date.value) - Date.parse(start_date.value)) / 1000 / 3600 / 24 ;
+    showPrice(duration);
+  }
+};
+
+
+const displayPrice = () => {
+  const start_date = document.getElementById('booking_start_date');
+  const end_date = document.getElementById('booking_end_date');
+  start_date.addEventListener('change', calculatePrice);
+  end_date.addEventListener('change', calculatePrice);
+}
+
+
+export { displayPrice };
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,4 +1,7 @@
 import "bootstrap";
 import"../plugins/flatpickr";
 import { hideFlashAfter5sec } from '../components/flashes';
+import { displayPrice } from '../components/total_price';
+
 hideFlashAfter5sec();
+displayPrice();
