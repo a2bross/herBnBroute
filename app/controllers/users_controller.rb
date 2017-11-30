3dfdd5f8f170383fa50a0f8735a4dab4c6ed7f7a
Nicolas Lizop <nicolas.lizop@gmail.com>
2017-11-30 12:42:40 +0100

    added accept decline complete logic to bookings

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,12 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     authorize @user
+    @user.bookings.each do |booking|
+      # check the proper Date object
+      if (booking.status == "accepted") && (booking.end_date < Date::today)
+        booking.status = "completed"
+      end
+    end
   end
 end
 
