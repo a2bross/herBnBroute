bdb012e90b52af75160ebe29dfbe6cfb5f54a7f3
Zoe Cade <zoe.cade@gmail.com>
2017-11-30 13:03:42 +0100

    added validation on capacity

--- app/models/plot.rb
+++ app/models/plot.rb
@@ -1,6 +1,7 @@
 class Plot < ApplicationRecord
   belongs_to :user
-  validates :name, :description, :capacity, :location, :user, presence: true
+  validates :name, :description, :capacity, :location, :user, :daily_price, presence: true
+  validates :capacity, numericality: { greater_than: 0 }
   mount_uploader :photo, PhotoUploader
   geocoded_by :location
   after_validation :geocode, if: :location_changed?
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
   has_many :plots, dependent: :destroy
+  has_many :bookings
+  has_many :bookings, through: :plots
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
