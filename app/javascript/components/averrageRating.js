026817a8ef0da7a9de4fbd3011e937f386fe3deb
Augustin de Brosses <augustin.debrosses@gmail.com>
2017-12-01 12:16:59 +0100

    flashes bug fix

---
+++ app/javascript/components/averrageRating.js
@@ -0,0 +1,8 @@
+
+///WIP///
+
+// const averageRate = (duration) => {
+//   const rating = document.getElementById('total_price');
+//   const averageRating = price.dataset.dailyprice;
+//   price.innerText = `Total price: ${daily_price * (duration + 1)}€`;
+// };
